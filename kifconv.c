dd6ba1d8c152e75b3291c07212b51b085b18c46b
Masq666 <koppin22@gmail.com>
2023-05-04 21:14:55 +0200

    Create kifconv.c
    Initial conversion tool based on the one from QOI.

---
+++ kifconv.c
@@ -0,0 +1,88 @@
+/*
+
+Copyright (c) 1998 - 2023, Philipe Rubio
+SPDX-License-Identifier: MIT
+
+Command line tool to convert between png <> kif format
+
+Requires:
+	-"stb_image.h" (https://github.com/nothings/stb/blob/master/stb_image.h)
+	-"stb_image_write.h" (https://github.com/nothings/stb/blob/master/stb_image_write.h)
+	-"kif.h" (https://github.com/Masq666/kif/blob/main/kif.h)
+
+Compile with: 
+	gcc kifconv.c -std=c99 -O3 -o kifconv
+
+*/
+
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_ONLY_PNG
+#define STBI_NO_LINEAR
+#include "stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#include "kif.h"
+
+#define STR_ENDS_WITH(S, E) (strcmp(S + strlen(S) - (sizeof(E)-1), E) == 0)
+
+int main(int argc, char **argv) {
+	if(argc < 3){
+		puts("Usage: kifconv <infile> <outfile>");
+		puts("Examples:");
+		puts("  kifconv input.png output.kif");
+		puts("  kifconv input.kif output.png");
+		exit(1);
+	}
+
+	void *pixels = NULL;
+	int w, h, channels, palEntries, RLEEntries;
+
+	if(STR_ENDS_WITH(argv[1], ".png")){
+		if(!stbi_info(argv[1], &w, &h, &channels)){
+			printf("Couldn't read header %s\n", argv[1]);
+			exit(1);
+		}
+
+		// Force all odd encodings to be RGBA
+		if(channels != 3){
+			channels = 4;
+		}
+
+		pixels = (void *)stbi_load(argv[1], &w, &h, NULL, channels);
+	}else if(STR_ENDS_WITH(argv[1], ".kif")){
+		KIFHeader desc;
+		pixels = kif_read(argv[1], &desc, 32);
+		channels = 4; //desc.bpp;
+		w = desc.Width;
+		h = desc.Height;
+		palEntries = desc.palEntries;
+	}
+
+	//printf("Height: %d Width: %d palEntries: %d Channels: %d\n", h, w, palEntries, channels);
+	
+	if(pixels == NULL){
+		printf("Couldn't load/decode %s\n", argv[1]);
+		exit(1);
+	}
+
+	int encoded = 0;
+
+	if(STR_ENDS_WITH(argv[2], ".png")){
+		encoded = stbi_write_png(argv[2], w, h, channels, pixels, 0);
+	}else if(STR_ENDS_WITH(argv[2], ".kif")){
+		encoded = kif_write(argv[2], pixels, &(KIFHeader){
+			.Width = w,
+			.Height = h
+		});
+	}
+
+	if(!encoded){
+		printf("Couldn't write/encode %s\n", argv[2]);
+		exit(1);
+	}
+
+	free(pixels);
+	return 0;
+}
